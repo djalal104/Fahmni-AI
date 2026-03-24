48128c78be517f9f40af689d789b0d69c1bde9f8
djalal104 <djalal104b@gmail.com>
2026-03-24 03:47:18 +0100

    add changes for hosting render + vercel

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,7 +6,7 @@
 
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: BASE_URL,
